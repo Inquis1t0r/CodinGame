d285d34125e11ed60452ca13928d681eb0d21819
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-06-22 22:51:13 +0200

    Create 91307463463c5163445f70c6e0543f929e0f69.cs

---
+++ 91307463463c5163445f70c6e0543f929e0f69.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine((x + y) * x);
+    }
+}
