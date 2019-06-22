f41b2736b7be639272c93a9e0d78d3598ab2e33a
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-06-22 23:21:20 +0200

    Create 913089b713b07ae3e21856aa42483751fdc6c6.cs

---
+++ 913089b713b07ae3e21856aa42483751fdc6c6.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string S = Console.ReadLine();
+
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+        
+        Console.WriteLine(S.ToCharArray().Where(c => c >= 'a' && c <= 'z').Count());
+    }
+}
