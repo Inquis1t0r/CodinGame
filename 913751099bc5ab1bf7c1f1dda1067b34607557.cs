6766eeea793d29ca817d9384ae0e8ade7d75287c
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-06-23 21:41:47 +0200

    Create 913751099bc5ab1bf7c1f1dda1067b34607557.cs

---
+++ 913751099bc5ab1bf7c1f1dda1067b34607557.cs
@@ -0,0 +1,1 @@
+//The Birds Language have only one rule : after each vowels(a,e,i,o,u,A,E,I,O,U) you should insert letter p and that vowels one more time. For a given String convert it to Birds Language.
