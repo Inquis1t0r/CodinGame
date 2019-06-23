48fd68af6a6b2332a5856618b912d604a47bed12
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-06-23 12:05:31 +0200

    Create 913409bcc9463a748ee4e60ed3b2c64acb4186.cs

---
+++ 913409bcc9463a748ee4e60ed3b2c64acb4186.cs
@@ -0,0 +1,28 @@
+//TODO:
+/*
+The program:
+Your program must read the string given on the standard input and print to the standard output the same string converted into 1337 speak.
+
+To convert text into 1337 speak, one must replace (whether upper or lower case):
+
+    'O' with '0'.
+    'L' with '1'.
+    'Z' with '2'.
+    'E' with '3'.
+    'A' with '4'.
+    'S' with '5'.
+    'G' with '6'.
+    'T' with '7'.
+    'B' with '8'.
+    'Q' with '9'.
+
+
+INPUT:
+A string S.
+
+OUTPUT:
+S converted to 1337 speak.
+
+CONSTRAINTS:
+S contains at least 1 character.
+*/
