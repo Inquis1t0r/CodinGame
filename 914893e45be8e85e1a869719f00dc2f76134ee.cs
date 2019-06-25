872684c72ac17be254104a61c951e3102f8acba3
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-06-25 12:58:08 +0200

    Create 914893e45be8e85e1a869719f00dc2f76134ee.cs

---
+++ 914893e45be8e85e1a869719f00dc2f76134ee.cs
@@ -0,0 +1,27 @@
+/* An odious number is defined as a number with an odd amount of 1s in its binary expansion. 4 is an example of this as it is "100" in binary and thus is an odious number.
+
+Output true or false depending on if the input, x, is odious or not. */
+
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine("true or false");
+    }
+}
