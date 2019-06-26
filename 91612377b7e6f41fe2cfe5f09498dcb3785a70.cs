fdee5aa32f8698f788812999149d0482a05a5d84
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-06-26 22:42:45 +0200

    Create 91612377b7e6f41fe2cfe5f09498dcb3785a70.cs

---
+++ 91612377b7e6f41fe2cfe5f09498dcb3785a70.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int A = int.Parse(Console.ReadLine());
+
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine( Math.Pow(A, 2) - A);
+    }
+}
