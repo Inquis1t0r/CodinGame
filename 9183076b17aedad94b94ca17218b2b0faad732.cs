5cdee33bad59792c76bea1f0b2eb40b0e9d704b7
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-06-29 17:53:34 +0200

    Create 9183076b17aedad94b94ca17218b2b0faad732.cs

---
+++ 9183076b17aedad94b94ca17218b2b0faad732.cs
@@ -0,0 +1,51 @@
+/*
+
+ Goal
+let us assume that you are a CID officer and got a mobile number of a criminal
+your task is to convey the message to higher officials in MORSE CODE as no one else can read it
+ie..;
+in morse code
+1 .---- 6 -....
+2 ..--- 7 --...
+3 ...-- 8 ---..
+4 ....- 9 ----.
+5 ..... 0 -----
+sample input and output
+1466886532 --> .---- ....- -.... -.... ---.. ---.. -.... ..... ...-- ..---
+NOTE: if it fails to be a mobile number print 'Untransformable'
+Input
+A string of phone number
+Output
+a string which contains every digit in phone number as morse code seperated by spaces
+Constraints
+no other characters except numbers are allowed
+Example
+Input
+
+1466886532
+
+Output
+
+.---- ....- -.... -.... ---.. ---.. -.... ..... ...-- ..---
+
+
+*/
+
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string mn = Console.ReadLine();
+    }
+}
