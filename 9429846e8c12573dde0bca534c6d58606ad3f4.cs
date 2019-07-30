adc96f15c9ff0923006b063b43113aaae8e927bf
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-07-30 14:15:48 +0200

    working solution
    i didn't make it in time tho :(

--- 9429846e8c12573dde0bca534c6d58606ad3f4.cs
+++ 9429846e8c12573dde0bca534c6d58606ad3f4.cs
@@ -15,5 +15,16 @@ class Solution
     {
         float diag = float.Parse(Console.ReadLine());
         string ratio = Console.ReadLine();
+
+        //float diag = 65;
+        //string ratio = "16:9";
+        int[] ratioTab = ratio.Split(':').Select(n => Convert.ToInt32(n)).ToArray();
+        float ratioCalc = (float)ratioTab[0] / ratioTab[1];
+
+        float height = diag / (float)Math.Sqrt((ratioCalc * ratioCalc) + 1);
+        float width = ratioCalc * height;
+
+        Console.WriteLine(width.ToString("0.00") + " x " + height.ToString("0.00"));
+
     }
 }
