5828f2fcd8831b688092dbceb364a05aed35779e
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-08-14 11:51:38 +0200

    Create 955920ca836a688d5232da4e95b1dd165de826.cs

---
+++ 955920ca836a688d5232da4e95b1dd165de826.cs
@@ -0,0 +1,27 @@
+//Your program must perform a binary XOR on two binary numbers given through the standard input and print the result to the standard output.
+
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string[] inputs = Console.ReadLine().Split(' ');
+        string n1 = inputs[0];
+        string n2 = inputs[1];
+
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine("binary digits");
+    }
+}
