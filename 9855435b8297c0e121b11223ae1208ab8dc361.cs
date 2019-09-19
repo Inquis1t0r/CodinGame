55a7076c479cb676f91912c211203d4816957d99
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-09-19 10:45:50 +0200

    Create 9855435b8297c0e121b11223ae1208ab8dc361.cs

---
+++ 9855435b8297c0e121b11223ae1208ab8dc361.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        string[] inputs = Console.ReadLine().Split(' ');
+        for (int i = 0; i < N; i++)
+        {
+            int xi = int.Parse(inputs[i]);
+        }
+
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine("result");
+    }
+}
