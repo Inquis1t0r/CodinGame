dc80b9bdb8e7043756c61baa00aaa175d4219555
Inquis1t0r <pi0tr3k9292@gmail.com>
2020-09-05 12:45:59 +0200

    Create 1366973710c8c93fc90d312e3435fd27ec96cae.cs

---
+++ 1366973710c8c93fc90d312e3435fd27ec96cae.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int M = int.Parse(Console.ReadLine());
+        int N = int.Parse(Console.ReadLine());
+
+        // Write an answer using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine("Answer");
+    }
+}
