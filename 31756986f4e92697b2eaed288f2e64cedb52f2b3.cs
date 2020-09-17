421e7ecdd033d2b16736fd4c929d290939df03de
Inquis1t0r <pi0tr3k9292@gmail.com>
2020-09-17 12:40:21 +0200

    Create 31756986f4e92697b2eaed288f2e64cedb52f2b3.cs

---
+++ 31756986f4e92697b2eaed288f2e64cedb52f2b3.cs
@@ -0,0 +1,48 @@
+/*
+
+ Goal
+When you go out to dinner with your friends, everyone toasts with everyone else once. You wonder how often one glass bumps into another.
+Input
+An integer n indicating the number of guests
+Output
+An integer that indicates the number of times the glasses have been knocked together
+Constraints
+0 ≤ n ≤ 1000
+Example
+Input
+
+2
+
+Output
+
+1
+
+
+*/
+
+
+
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        // Write an answer using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+        //TODO:https://allagora.wordpress.com/2013/10/03/the-toast-formula/
+
+        Console.WriteLine("1");
+    }
+}
